310f5019e77b20cce93889b98bd159a9bc68428d
MohamadHelli <mou.hamad123@hotmail.com>
2022-06-09 15:17:45 +0200

    advnce

--- Mohamad/Advance/script.ts
+++ Mohamad/Advance/script.ts
@@ -1,3 +1,5 @@
+const cars: Array<Vehicles> = [];
+
 class Vehicles { 
     model : string; 
     kilometers : Number; 
@@ -10,9 +12,12 @@ class Vehicles {
         this.fuel  = fuel ;
         this.year = year;
         this.img = img;
+        cars.push(this);
     }
     printcar() { 
-    return `<div class="card" style="width: 18rem;">
+    return `
+    <div class = "col-4">
+    <div class="card" style="width: 18rem;">
     <img src="${this.img}" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Model :${this.model} </h5>
@@ -20,11 +25,22 @@ class Vehicles {
         <p class="card-text">fuel type : ${this.fuel} </p>
         <p class="card-text">kilometers : ${this.kilometers }</p>
         <button class="btn btn-info">See Price</button>
-    </div>`; 
+    </div>
+    <div>
+    `; 
     }
 }
-let car = new Vehicles("BMW", 6000,"diesel", "2021,01,01", "https://cdn.pixabay.com/photo/2019/10/19/03/50/bmw-4560531__340.jpg");
-let car1 = new Vehicles("Ferrari", 10000,"benzin", "2020,01,01", "https://cdn.pixabay.com/photo/2020/02/20/18/43/car-4865483_960_720.jpg");
-(document.getElementById("card") as HTMLElement).innerHTML = car.printcar();
-(document.getElementById("card") as HTMLElement).innerHTML += car1.printcar();
+
+
+
+new Vehicles("BMW", 6000,"diesel", "2021,01,01", "https://cdn.pixabay.com/photo/2019/10/19/03/50/bmw-4560531__340.jpg");
+new Vehicles("Ferrari", 10000,"benzin", "2020,01,01", "https://cdn.pixabay.com/photo/2020/02/20/18/43/car-4865483_960_720.jpg");
+new Vehicles("BMW", 6000,"diesel", "2021,01,01", "https://cdn.pixabay.com/photo/2019/10/19/03/50/bmw-4560531__340.jpg");
+new Vehicles("Ferrari", 10000,"benzin", "2020,01,01", "https://cdn.pixabay.com/photo/2020/02/20/18/43/car-4865483_960_720.jpg");
+// (document.getElementById("card") as HTMLElement).innerHTML = car.printcar();
+// (document.getElementById("card") as HTMLElement).innerHTML += car1.printcar();
+
+for (let val of cars ) {
+    (document.getElementsByClassName("ruf")[0] as HTMLElement).innerHTML += val.printcar();
+}
 
