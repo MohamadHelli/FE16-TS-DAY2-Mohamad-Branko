374660df15b55a78f47887b9b2071d888dc7f7a9
MohamadHelli <mou.hamad123@hotmail.com>
2022-06-09 11:01:27 +0200

    Basic

--- Mohamad/Basic/script.js
+++ Mohamad/Basic/script.js
@@ -1,2 +1,25 @@
 "use strict";
-let num1 = 10;
+class Cart {
+    constructor(name, age, jobTitle) {
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+    }
+    printcart() {
+        return `Hello there, My name is  ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle}`;
+    }
+}
+let cart1 = new Cart("Mohamad", 29, "IT");
+document.getElementById("cart").innerHTML = cart1.printcart();
+class Person extends Cart {
+    constructor(name, age, jobTitle, salary, jobLocation) {
+        super(name, age, jobTitle);
+        this.salary = salary;
+        this.jobLocation = jobLocation;
+    }
+    printcart1() {
+        return `Hello there, My name is  ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle} and I get ${this.salary} every month, and I work in ${this.jobLocation}`;
+    }
+}
+let person1 = new Person("Mohamad", 29, "IT", 2000, "Vienna");
+document.getElementById("cartnew").innerHTML = person1.printcart1();
--- Mohamad/Basic/script.ts
+++ Mohamad/Basic/script.ts
@@ -1,1 +1,31 @@
-let num1 : Number = 10;
+class Cart{ 
+    name : string; 
+    age : Number; 
+    jobTitle: string; 
+    constructor(name: string, age: Number, jobTitle: string) { 
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+    }
+    printcart() { 
+        return `Hello there, My name is  ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle}`; 
+    }
+}
+let cart1 = new Cart("Mohamad", 29,"IT");
+(document.getElementById("cart") as HTMLElement).innerHTML = cart1.printcart()
+
+class Person extends Cart{
+    salary : Number ;
+    jobLocation : string;
+    constructor(name:string, age: Number, jobTitle: string, salary : Number ,jobLocation : string ){
+    super(name, age ,jobTitle );
+    this.salary=salary;
+    this.jobLocation =jobLocation ;
+}
+    printcart1() { 
+    return `Hello there, My name is  ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle} and I get ${this.salary} every month, and I work in ${this.jobLocation}`; 
+    }
+}
+let person1 = new Person("Mohamad", 29 , "IT" , 2000, "Vienna");
+(document.getElementById("cartnew") as HTMLElement).innerHTML = person1.printcart1()
+
